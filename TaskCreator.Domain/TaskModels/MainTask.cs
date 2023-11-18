5629d4f94db8735a9353893d2e2cf3b3b5005b6c
artur <zeroone02@mail.ru>
2023-11-18 15:37:06 +0300

    wip

--- TaskCreator.Domain/TaskModels/MainTask.cs
+++ TaskCreator.Domain/TaskModels/MainTask.cs
@@ -1,8 +1,19 @@
 ﻿using TaskCreator.DDD;
 
 namespace TaskCreator.Domain;
- public class MainTask : AggregateRoot<int>
+ public  class MainTask : AggregateRoot<int>
 {
+    public MainTask(MainTask mainTask)
+    {
+        Name = mainTask.Name;
+        Description = mainTask.Description;
+    }
+    public MainTask(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+    
     public string Name { get; protected set; }
     public string Description { get; protected set; }
 
--- TaskCreator.Domain/TaskModels/SideTask.cs
+++ TaskCreator.Domain/TaskModels/SideTask.cs
@@ -4,9 +4,21 @@ using TaskCreator.DDD;
 namespace TaskCreator.Domain;
 public class SideTask : Entity<int>
 {
-    public int MainTaskId { get; set; }
+    public SideTask(SideTask sideTask)
+    {
+        MainTaskId = sideTask.MainTaskId;
+        Name = sideTask.Name;
+        Description = sideTask.Description;
+    }
+    public SideTask(int mainTaskId,string name,string description)
+    {
+        MainTaskId = mainTaskId;
+        Name = name;
+        Description = description;
+    }
+    public int MainTaskId { get; protected set; }
     [ForeignKey("MainTaskId")]
-    public MainTask MainTask { get; set; }
+    public MainTask MainTask { get; protected set; }
     public string Name { get; protected set; }
     public string Description { get; protected set; }
 }
